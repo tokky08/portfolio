5fe76842a1a72b0f06572b68b6010b90960282e4
tokky08 <tokky.kz08@gmail.com>
2021-07-23 22:22:03 +0900

    Experienceを追加

--- components/BasicTimeline.jsx
+++ components/BasicTimeline.jsx
@@ -5,6 +5,9 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
 // import { makeStyles } from '@material-ui/core/styles';
 // import styles from '../styles/BasicTimeline.module.scss';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
@@ -12,17 +15,12 @@ import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 export default function BasicTimeline() {
     const theme = createMuiTheme({
         overrides: {
-            // MuiTimelineItem: {
-            //     root: {
-            //         display: "block"
-            //     }
-            // },
             MuiTimelineItem: {
                 missingOppositeContent: {
                     '&::before': {
-                        flex: "none"
+                        flex: "none",
+                        padding: "0"
                     },
-                    // backgroundColor: "#ff9999",
                 },
             },
         },
@@ -30,26 +28,125 @@ export default function BasicTimeline() {
 
     return (
         <ThemeProvider theme={theme}>
+            <Typography variant="h5" component="h2">Experience</Typography>
             <Timeline>
                 <TimelineItem>
                     <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
+                        <TimelineDot />
+                        <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Eat</TimelineContent>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2018.04
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                京都産業大学
+                            </Box>
+                        </Typography>
+                        <Typography>情報理工学部に入学。</Typography>
+                    </TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
+                        <TimelineDot />
+                        <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Code</TimelineContent>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2020.03~現在
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                株式会社ソフィアソリューションズ
+                            </Box>
+                        </Typography>
+                        <Typography>サーバーサイドを担当。主にPHP, Python, AWSでのシステム開発。たまにフロントのコーディング業務も。</Typography>
+                    </TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineSeparator>
-                    <TimelineDot />
+                        <TimelineDot />
+                        <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Sleep</TimelineContent>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2020.09
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                株式会社いい生活
+                            </Box>
+                        </Typography>
+                        <Typography>インターン。vue.jsと社内APIを用いて物件探しサービスを開発。</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2020.09
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                ソフトバンク株式会社
+                            </Box>
+                        </Typography>
+                        <Typography>ハッカソン。準優勝。SDGsをテーマに学生と研究室を繋げるプラットフォームを開発。Flask, AWSを使用。</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2021.06~07
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                ヤフー株式会社
+                            </Box>
+                        </Typography>
+                        <Typography>内定者限定のハッカソン。レストランが決まらない時に使うwebサービスを開発。Flask, React, GCPを使用。</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2022.03
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                京都産業大学
+                            </Box>
+                        </Typography>
+                        <Typography>学部で卒業予定。</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <Typography variant="body2" color="textSecondary">
+                            2022.04
+                        </Typography>
+                        <Typography component="div">
+                            <Box fontWeight="fontWeightBold">
+                                ヤフー株式会社
+                            </Box>
+                        </Typography>
+                        <Typography>新卒入社予定。</Typography>
+                    </TimelineContent>
                 </TimelineItem>
             </Timeline>
         </ThemeProvider>

041bad8471ff945a3553e03ebb7a8bc7b028465d
tokky08 <tokky.kz08@gmail.com>
2021-07-24 14:29:20 +0900

    ヘッダーを追跡するように変更

--- components/ButtonAppBar.jsx
+++ components/ButtonAppBar.jsx
@@ -5,6 +5,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Box from '@material-ui/core/Box';
 
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -21,11 +25,39 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function ButtonAppBar() {
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ButtonAppBar(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
+      <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
       <AppBar position="static" className={classes.bar}>
         <Toolbar display="flex" p={1}>
           <Box p={1} flexGrow={1}>
@@ -36,6 +68,8 @@ export default function ButtonAppBar() {
           </Box>
         </Toolbar>
       </AppBar>
+      </ElevationScroll>
+      </React.Fragment>
     </div>
   );
 }

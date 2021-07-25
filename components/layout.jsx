ccb04cf0d9d939e63a3c7186ce8dfc4119096940
tokky08 <tokky.kz08@gmail.com>
2021-07-25 15:49:17 +0900

    twitter:og:img修正

--- components/layout.jsx
+++ components/layout.jsx
@@ -26,12 +26,12 @@ export default function Layout({ children, home }) {
         <meta name="twitter:site" content="@tokky08" /> */}
         <meta name="keywords" content="ポートフォリオ,エンジニア,tokky08,とっきー," />
 
-        <meta property="og:url" content="https://tokky08.dev/"/>
+        <meta property="og:url" content="https://tokky08.dev"/>
         <meta property="og:type" content="website" />
         <meta property="og:title" content="tokky08 | ポートフォリオ" />
-        <meta property="og:description" content="どうも、tokky08です。こちらはtokky08のポートフォリオです。外部リンクを合わせてこのサイトを見れば私の全てが分かります。" />
+        <meta property="og:description" content="tokky08のポートフォリオです" />
         <meta property="og:site_name" content="tokky08.dev" />
-        <meta property="og:image" content="/images/profile.png" />
+        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1343388061653106689/G7r2wNV8_400x400.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tokky08" />
       </Head>

265db874c93df9b37455bb7509e175681fe0f338
tokky08 <tokky.kz08@gmail.com>
2021-07-25 15:57:00 +0900

    update

--- components/layout.jsx
+++ components/layout.jsx
@@ -28,10 +28,10 @@ export default function Layout({ children, home }) {
 
         <meta property="og:url" content="https://tokky08.dev"/>
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="tokky08 | ポートフォリオ" />
+        <meta property="og:title" content="ポートフォリオ | tokky08" />
         <meta property="og:description" content="tokky08のポートフォリオです" />
         <meta property="og:site_name" content="tokky08.dev" />
-        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1343388061653106689/G7r2wNV8_400x400.png" />
+        <meta property="og:image" content={"https://pbs.twimg.com/profile_images/1343388061653106689/G7r2wNV8_400x400.png"} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tokky08" />
       </Head>
